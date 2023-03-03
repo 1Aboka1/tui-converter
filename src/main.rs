6251d22d0375702a8b80d5766e8eafa47340202b
1Aboka1 <zz.abulhair.zz@gmail.com>
2023-03-03 10:47:36 +0600

    styling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+mod ui;
+use std::{io::{self, Write}, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend, 
+    Terminal
+};
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}, QueueableCommand, cursor
+};
+use ui::ui::draw_init;
 
 fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
+    start_ui()?;
+
+    Ok(())
+}
+
+fn start_ui() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    
+    terminal.draw(|f| {
+        draw_init(f).expect("Couldn't draw");
+    })?;
+
+    thread::sleep(Duration::from_millis(3000));
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
     Ok(()) 
 }
